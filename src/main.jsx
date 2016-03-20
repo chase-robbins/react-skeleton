3856a4565dfe135c43396ee1583e50e91b8be64a
Chase Robbins <chaserobbins@me.com>
2016-03-19 18:40:37 -0700

    List app

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require('./components/List.jsx');
+
+reactDOM.render(<List />, document.getElementById('ingredients'));
